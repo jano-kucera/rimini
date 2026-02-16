c81b9f06ac0b21659a75c467c27bf314fe6a1424
jano-kucera <janislavkucera@gmail.com>
2026-02-16 17:45:16 +0100

    Calendar, price and dates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { AfterViewInit, Component, signal } from '@angular/core';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatCalendar } from '@angular/material/datepicker';
 import { RouterOutlet } from '@angular/router';
+import { Calendar } from 'vanilla-calendar-pro';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +10,26 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements AfterViewInit {
   protected readonly title = signal('rimini');
+
+  ngAfterViewInit(): void {
+    const from = '2026-07-26';
+    const to = '2026-08-02';
+    const calendar = new Calendar("#calendar", {
+      dateMin: from,
+      dateMax: to,
+      displayDisabledDates: false,
+      disableAllDates: true,
+      locale: 'sk-SK',
+      selectedMonth: 6,
+      selectedWeekends: [],
+      selectedTheme: "light",
+      selectionDatesMode: "multiple-ranged",
+      selectedDates: [from, to],
+      type: 'default',
+    });
+
+    calendar.init();
+  }
 }
